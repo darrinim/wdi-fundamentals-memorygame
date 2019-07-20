fa29971203067092d87132fc8c38b6a0e0c09f78
Darrin Im <darrinim@Darrins-MacBook-Pro.local>
2019-07-20 12:40:28 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,10 +1,27 @@
 console.log("Up and running!");
+cardsInPlay = [];
+
+const cards = ["queen", "queen", "king", "king"];
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
 
-var cardOne = 'queen';
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+console.log(cardsInPlay);
+
+
+cardsInPlay = [];
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert('You found a match!');
+}	else {
+	alert('Sorry, try again.')
+}
+
+
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+console.log("User flipped " + cardTwo);
