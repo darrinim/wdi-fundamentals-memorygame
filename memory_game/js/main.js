0c6fabef5e73075ecefc272c3263b50cd47526df
Darrin Im <darrinim@Darrins-MBP.home>
2019-07-20 16:33:02 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,27 +1,49 @@
 console.log("Up and running!");
-cardsInPlay = [];
+
 
 const cards = ["queen", "queen", "king", "king"];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
+// var cardOne = cards[0];
+// var cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+// cardsInPlay.push(cardOne);
+// cardsInPlay.push(cardTwo);
 
 
-console.log(cardsInPlay);
+// console.log(cardsInPlay);
 
 
 cardsInPlay = [];
 
-if (cardsInPlay[0] === cardsInPlay[1]) {
+// cardsInPlay.push(cards[cardId]);
+
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+};
+
+function flipCard (cardId) {
+	checkForMatch();
+	if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert('You found a match!');
 }	else {
 	alert('Sorry, try again.')
 }
+	console.log("User flipped " + cards[cardId]);
+};
+
+flipCard(2);
 
 
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+
+cardsInPlay.push(cards[cardId]);
+
+// if (cardsInPlay[0] === cardsInPlay[1]) {
+// 	alert('You found a match!');
+// }	else {
+// 	alert('Sorry, try again.')
+// }
